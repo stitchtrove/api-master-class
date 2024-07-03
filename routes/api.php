b63e1bc5e02beeab0b34cb5e412b369a6ad01490
stitchtrove <stitchtrove@gmail.com>
2024-07-03 14:09:21 +0100

    models, migrations and seeders

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,7 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function () {
+    return Ticket::all();
+});
