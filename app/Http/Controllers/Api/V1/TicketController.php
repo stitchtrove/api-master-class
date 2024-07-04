d9fd6f5ecbb78612cbbc59d11d9f0d1453736929
stitchtrove <stitchtrove@gmail.com>
2024-07-04 16:19:52 +0100

    store ticket functionality

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Models\User;
 use App\Models\Ticket;
 use App\Http\Filters\V1\TicketFilter;
 use App\Http\Resources\V1\TicketResource;
+use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class TicketController extends ApiController
 {
@@ -37,6 +40,28 @@ class TicketController extends ApiController
             return new TicketResource($ticket->load('user'));
         }
         return new TicketResource($ticket);
+    }
+
+    /**
+     * Store the specified resource in storage.
+     */
+    public function store(StoreTicketRequest $request)
+    {
+        try {
+            $user = User::findOrFail($request->input('data.relationships.author.data.id'));
+        } catch (ModelNotFoundException $exception) {
+            return $this->ok('User not found', [
+                'error' => 'The provided user id does not exist.'
+            ]);
+        }
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $request->input('data.relationships.author.data.id'),
+        ];
+
+        return new TicketResource(Ticket::create($model));
     }
 
     /**
